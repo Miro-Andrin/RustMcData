8c4557d85446c12468f45e781a0c2eb9c4f77588
Miro <miro.sveits@gmail.com>
2021-09-19 00:31:08 +0200

    Added map icons

--- src/models/map_icons.rs
+++ src/models/map_icons.rs
@@ -1,1 +1,40 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(transparent)]
+pub struct MapIcons {
+    icons: Vec<MapIcon>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MapIcon {
+    id : u64,
+    name: String,
+    /// Description of the map icon's appearance
+    appearance: String,
+    #[serde(rename = "visibleInItemFrame")]
+    visible_in_item_frame: bool,
+}
+
+
+#[cfg(test)]
+mod test {
+    
+    use super::*;
+    const MC_DATA_DIR: &str = "./minecraft-data/data/pc/";
+
+    #[test]
+    fn test_block_loot() {
+        for version_folder in std::fs::read_dir(MC_DATA_DIR).unwrap() {
+            let dir = version_folder.unwrap();
+            let mut path = dir.path();
+            path.push("mapIcons.json");
+
+            if path.exists() {
+                println!("{}",path.display());
+                let contents = std::fs::read_to_string(path).unwrap();
+                let _shapes: MapIcons = serde_json::from_str(&contents).unwrap();
+            }
+        }
+    }
+}
