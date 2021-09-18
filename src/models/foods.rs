5d62886e1d692b84238e5e21905e0573d9be6e51
Miro <miro.sveits@gmail.com>
2021-09-19 00:02:30 +0200

    Added foods

--- src/models/foods.rs
+++ src/models/foods.rs
@@ -1,1 +1,55 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(transparent)]
+pub struct Foods {
+    foods: Vec<Food>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Food {
+    id: u64,
+    #[serde(rename = "displayName")]
+    display_name: String,
+    #[serde(rename = "stackSize")]
+    stack_size: u64,
+    name: String,
+    #[serde(rename = "foodPoints")]
+    food_points: f64, // zero or more
+    saturation: f64,  // zero or more
+    #[serde(rename = "effectiveQuality")]
+    effective_quality: f64,
+    #[serde(rename = "saturationRatio")]
+    saturation_ratio: f64,
+    variations: Option<Vec<Variation>>
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Variation {
+    metadata: u64,
+    #[serde(rename = "displayName")]
+    display_name: String,
+}
+
+
+#[cfg(test)]
+mod test {
+    
+    use super::*;
+    const MC_DATA_DIR: &str = "./minecraft-data/data/pc/";
+
+    #[test]
+    fn test_block_loot() {
+        for version_folder in std::fs::read_dir(MC_DATA_DIR).unwrap() {
+            let dir = version_folder.unwrap();
+            let mut path = dir.path();
+            path.push("foods.json");
+
+            if path.exists() {
+                println!("{}",path.display());
+                let contents = std::fs::read_to_string(path).unwrap();
+                let _shapes: Foods = serde_json::from_str(&contents).unwrap();
+            }
+        }
+    }
+}
