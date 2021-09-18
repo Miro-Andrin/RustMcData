f11584795f60577486a8f6ac4c968d6ee1e77705
David Alasow <davidalasow@gmail.com>
2021-09-19 00:39:23 +0200

    Added language.

--- src/models/language.rs
+++ src/models/language.rs
@@ -1,1 +1,30 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(transparent)]
+pub struct Language {
+    mapping: HashMap<String, String>,
+}
+
+mod test {
+    use super::*;
+    const MC_DATA_DIR: &str = "./minecraft-data/data/pc/";
+
+    #[test]
+    fn test_language() {
+        for version_folder in std::fs::read_dir(MC_DATA_DIR).unwrap() {
+            let dir = version_folder.unwrap();
+            let mut path = dir.path();
+            path.push("language.json");
+
+            if path.exists() {
+                println!("{}",path.display());
+                let contents = std::fs::read_to_string(path).unwrap();
+                let _language: Language = serde_json::from_str(&contents).unwrap();
+            }
+        }
+    }
+}
 
